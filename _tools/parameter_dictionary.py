06a8dc4094b8092fa563e35b3b13f6edabe6136e
mikarubi <mikarubi@gmail.com>
2019-11-09 15:22:30 -0500

    simplified parameter dictionary

--- _tools/parameter_dictionary.py
+++ _tools/parameter_dictionary.py
@@ -1,42 +1,23 @@
 def parameter_dictionary():
-    '''return parameter dictionary with some specified defaults'''
+    '''parameter dictionary with specified defaults'''
     
-    return \
-            {
-                # type of registration: possible values -- 'rigid', 'translation', None
-                'registration': 'rigid',
-                # cell diameter (microns)
-                'diam_cell': 6.0,
-                # path to ANTs directory
-                'dir_ants': '',
-                # path to image directory
-                'dir_input': '',
-                # path to output directory
-                'dir_output': '',
-                # spatial coarse-graining in x-y
-                'ds': 2,
-                # temporal downsampling for cell detection (ignored if specified timepoints)
-                'dt': 1,
-                # timepoints to use in cell detection (computed from dt if empty)
-                'timepoints': None,
-                # frequency for high-pass filtering of cell timeseries (Hz)
-                'f_hipass': 0,
-                # frequency at which volumes are acquired (Hz)
-                'f_volume': 2.66,
-                # number of cells in block
-                'n_cells_block': 100,
-                # number of brain in each image (2 in two-color images)
-                'n_colors': 1,
-                # x resolution (microns)
-                'res_x': 0.40625,
-                # y resolution (microns)
-                'res_y': 0.40625,
-                # z resolution (microns)
-                'res_z': 1.0,
-                # time interval for baseline calculation (seconds)
-                't_baseline': 300,
-                # exposure time: time at which each slice is acquired (seconds)
-                't_section': 0.01,
-                # intensity threshold for masking volumes
-                'thr_mask': 0.5,
+    return  {
+              'registration': 'rigid',  # type of registration: 'rigid', 'translation' or None
+              'diam_cell': 6.0,         # cell diameter (microns)
+              'dir_ants': '',           # path to ANTs directory
+              'dir_input': '',          # path to image directory
+              'dir_output': '',         # path to output directory
+              'ds': 2,                  # spatial coarse-graining in x-y dimension
+              'dt': 1,                  # temporal downsampling for cell detection (ignored if 'timepoints' specified)
+              'timepoints': None,       # timepoints to use in cell detection (computed from dt if not specified)
+              'f_hipass': 0,            # frequency (Hz) for high-pass filtering of cell timeseries
+              'f_volume': 2.66,         # imaging frequency (Hz)
+              'n_cells_block': 100,     # number of cells in block
+              'n_colors': 1,            # number of brain colors (2 in two-color images)
+              'res_x': 0.40625,         # x resolution (microns)
+              'res_y': 0.40625,         # y resolution (microns)
+              'res_z': 1.0,             # z resolution (microns)
+              't_baseline': 300,        # interval for baseline calculation (seconds)
+              't_section': 0.01,        # exposure time (seconds): time of slice acquisition
+              'thr_mask': 0.5,          # threshold for volume mask (0 to 1: least to most aggressive mask)
             }
