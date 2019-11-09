b6cda711863e31e3590bf791a1838cb92467c437
mikarubi <mikarubi@gmail.com>
2019-11-09 16:06:39 -0500

    minor changes

--- _steps/step0.py
+++ _steps/step0.py
@@ -1,40 +1,42 @@
-def process_parameters(parameters=None):
-    '''create parameter file'''
+def process_parameters(parameters0=None):
+    '''process parameters and create parameter file'''
     
     import os
+    import copy
     import pickle
     import numpy as np
     from voluseg._tools.parameter_dictionary import parameter_dictionary
-    from voluseg._tools.load_parameters import load_parameters
     
-    flag = 0
-    if not parameters:
-        print('error: specify parameter dictionary as input')
-        flag = 1
+    parameters = copy.deepcopy(parameters0)
+    
+    if not type(parameters) == dict:
+        print('error: specify parameter dictionary as input.')
+        return
         
+    if not ('dir_input' in parameters) or not ('dir_output' in parameters):
+        print('error: specify dir_input and dir_output dictionary keys.')
+        return
+    
+    # get input and output directories, and parameter filename
+    dir_input = parameters['dir_input']
+    dir_output = parameters['dir_output']
+    filename_parameters = os.path.join(dir_output, 'parameters.pickle')
+    
+    # load parameteres from file, if it already exists
+    if os.path.isfile(filename_parameters):
+        print('exiting, parameter file exists: %s.'%(filename_parameters))
+        return
+    
+    # check if any parameters are missing
     missing_parameters = set(parameter_dictionary()) - set(parameters)    
     if missing_parameters:
         print('error: missing parameters %s.'%(', '.join(missing_parameters)))
-        flag = 1
-
-    dir_input = parameters['dir_input']
-    dir_output = parameters['dir_output']
-    
-    # configure only if prepro_parameters doesn't exist
-    filename_parameters = os.path.join(dir_output, 'parameters.pickle')
-    if os.path.isfile(filename_parameters):
-        print('loading existing parameters from %s.'%(filename_parameters))
-        parameters = load_parameters(filename_parameters)
-        flag = 1
-        
+        return
+           
     # check registration
     if not parameters['registration'] in ['rigid', 'translation', None]:
         print('error: \'registration\' must be either \'rigid\', \'translation\' or None.')
-        flag = 1
-        
-    # if flag, then return early
-    if flag:
-        return parameters
+        return
         
     # get image extension, image names and number of segmentation timepoints
     file_names = [i.split('.', 1) for i in os.listdir(dir_input) if '.' in i]
@@ -67,11 +69,9 @@ def process_parameters(parameters=None):
     try:
         os.makedirs(dir_output, exist_ok=True)
         with open(filename_parameters, 'wb') as file_handle:
-            pickle.dump(parameters, file_handle)
-        
-            print('parameter file successfully saved.')        
+            pickle.dump(parameters, file_handle)        
+            print('parameter file successfully saved.')
             
     except Exception as msg:
         print('parameter file not saved: %s.'%(msg))
-        
-    return parameters
+                
--- _tools/parameter_dictionary.py
+++ _tools/parameter_dictionary.py
@@ -11,12 +11,12 @@ def parameter_dictionary():
               'dt': 1,                  # temporal downsampling for cell detection (ignored if 'timepoints' specified)
               'timepoints': None,       # timepoints to use in cell detection (computed from dt if not specified)
               'f_hipass': 0,            # frequency (Hz) for high-pass filtering of cell timeseries
-              'f_volume': 2.66,         # imaging frequency (Hz)
+              'f_volume': 2.0,          # imaging frequency (Hz)
               'n_cells_block': 100,     # number of cells in block
               'n_colors': 1,            # number of brain colors (2 in two-color images)
               'res_x': 0.40625,         # x resolution (microns)
               'res_y': 0.40625,         # y resolution (microns)
-              'res_z': 1.0,             # z resolution (microns)
+              'res_z': 5.0,             # z resolution (microns)
               't_baseline': 300,        # interval for baseline calculation (seconds)
               't_section': 0.01,        # exposure time (seconds): time of slice acquisition
               'thr_mask': 0.5,          # threshold for volume mask (0 to 1: least to most aggressive mask)
