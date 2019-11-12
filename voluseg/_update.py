8dfe6ecf4e1f51f60e9dc648530f6230805f01d9
mikarubi <mikarubi@gmail.com>
2019-11-12 11:43:39 -0500

    fix update

--- voluseg/_update.py
+++ voluseg/_update.py
@@ -1,9 +1,5 @@
 def update():
     '''update package'''
     
-    import pip
-    
-    try:
-        pip._internal.main(['install', 'git+https://github.com/mikarubi/voluseg.git', '--upgrade'])
-    except:
-        pip.main(['install', 'git+https://github.com/mikarubi/voluseg.git', '--upgrade'])
+    import os
+    os.system(['pip install --upgrade git+https://github.com/mikarubi/voluseg.git'])
