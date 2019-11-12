603a8b2aed4a711a36d65de648cf1c3d9049103d
mikarubi <mikarubi@gmail.com>
2019-11-11 21:12:58 -0500

    fix setup

--- voluseg/__init__.py
+++ voluseg/__init__.py
@@ -1,12 +1,12 @@
-#from voluseg._steps.step0 import process_parameters as step0_process_parameters
-#from voluseg._steps.step1 import process_images as step1_process_images
-#from voluseg._steps.step2 import align_images as step2_align_images
-#from voluseg._steps.step3 import mask_images as step3_mask_images
-#from voluseg._steps.step4 import detect_cells as step4_detect_cells
-#from voluseg._steps.step5 import clean_cells as step5_clean_cells
-#
-#from voluseg._tools.parameter_dictionary import parameter_dictionary
-#from voluseg._tools.load_parameters import load_parameters
-#from voluseg._tools.load_metadata import load_metadata
-#
-#from voluseg._update import update as update
+from voluseg._steps.step0 import process_parameters as step0_process_parameters
+from voluseg._steps.step1 import process_images as step1_process_images
+from voluseg._steps.step2 import align_images as step2_align_images
+from voluseg._steps.step3 import mask_images as step3_mask_images
+from voluseg._steps.step4 import detect_cells as step4_detect_cells
+from voluseg._steps.step5 import clean_cells as step5_clean_cells
+
+from voluseg._tools.parameter_dictionary import parameter_dictionary
+from voluseg._tools.load_parameters import load_parameters
+from voluseg._tools.load_metadata import load_metadata
+
+from voluseg._update import update as update
