3d7a1468ff756ad70a6adc49b3194bb26ce2aadc
mikarubi <mikarubi@gmail.com>
2021-03-14 08:26:42 -0400

    bug fixes

--- voluseg/_steps/step5.py
+++ voluseg/_steps/step5.py
@@ -85,14 +85,19 @@ def clean_cells(parameters):
             timeseries = timeseries_tuple[1]
             return clean_signal(bparameters.value, timeseries)
         if p.parallel_clean:
-            print('Computing baseline in parallel mode.')
+            print('Computing baseline in parallel mode... ', end='')
             timebase = evenly_parallelize(cell_timeseries).map(get_timebase).collect()
         else:
-            print('Computing baseline in serial mode.')
+            print('Computing baseline in serial mode... ', end='')
             timeseries_tuple = zip([[]]*len(cell_timeseries), cell_timeseries)
             timebase = map(get_timebase, timeseries_tuple)
+        print('done.')
         
         cell_timeseries1, cell_baseline1 = list(zip(*timebase))
+        
+        # convert to arrays
+        cell_timeseries1 = np.array(cell_timeseries1)
+        cell_baseline1 = np.array(cell_baseline1)
         
         # check that all series are in single precision
         assert(cell_weights.dtype=='float32')
@@ -126,8 +131,8 @@ def clean_cells(parameters):
             file_handle['volume_weight'] = volume_weight
             file_handle['cell_weights'] = cell_weights
             file_handle['cell_timeseries_raw'] = cell_timeseries
-            file_handle['cell_timeseries'] = np.array(cell_timeseries1)
-            file_handle['cell_baseline'] = np.array(cell_baseline1)
+            file_handle['cell_timeseries'] = cell_timeseries1
+            file_handle['cell_baseline'] = cell_baseline1
             file_handle['background'] = background
 
     # clean up
