3958e40d6200fec2a9dc1d8eaea0e5229ddcefe5
mikarubi <mikarubi@gmail.com>
2021-04-07 13:54:30 -0400

    rename block_id

--- voluseg/_steps/step4e.py
+++ voluseg/_steps/step4e.py
@@ -38,7 +38,7 @@ def collect_blocks(color_i, parameters):
     def add_data(tuple_ii):
         ii = tuple_ii[1]
         try:
-            block_id = []
+            cell_block_id = []
             cell_xyz = []
             cell_weights = []
             cell_timeseries = []
@@ -46,15 +46,15 @@ def collect_blocks(color_i, parameters):
             fullname_block = os.path.join(dir_cell, 'block%05d'%(ii))
             with h5py.File(fullname_block+hdf, 'r') as file_handle:
                 for ci in range(file_handle['n_cells'][()]):
-                    block_id.append(ii)
+                    cell_block_id.append(ii)
                     cell_xyz.append(file_handle['/cell/%05d/xyz'%(ci)][()])
                     cell_weights.append(file_handle['/cell/%05d/weights'%(ci)][()])
                     cell_timeseries.append(file_handle['/cell/%05d/timeseries'%(ci)][()])
 
             if p.parallel_clean:
-                cell_data.add([block_id, cell_xyz, cell_weights, cell_timeseries])
+                cell_data.add([cell_block_id, cell_xyz, cell_weights, cell_timeseries])
             else:
-                return [block_id, cell_xyz, cell_weights, cell_timeseries]
+                return [cell_block_id, cell_xyz, cell_weights, cell_timeseries]
 
         except KeyError:
             print('block %d is empty.'%ii)
@@ -63,19 +63,19 @@ def collect_blocks(color_i, parameters):
 
     if p.parallel_clean:
         evenly_parallelize(np.argwhere(block_valids).T[0]).foreach(add_data)
-        block_id, cell_xyz, cell_weights, cell_timeseries = cell_data.value
+        cell_block_id, cell_xyz, cell_weights, cell_timeseries = cell_data.value
     else:
         idx_block_valids = np.argwhere(block_valids).T[0]
         valids_tuple = zip([[]]*len(idx_block_valids), idx_block_valids)
-        block_id, cell_xyz, cell_weights, cell_timeseries = list(zip(*map(add_data, valids_tuple)))
-        block_id = [ii for bi in block_id for ii in bi]
+        cell_block_id, cell_xyz, cell_weights, cell_timeseries = list(zip(*map(add_data, valids_tuple)))
+        cell_block_id = [ii for bi in cell_block_id for ii in bi]
         cell_xyz = [xyzi for ci in cell_xyz for xyzi in ci]
         cell_weights = [wi for ci in cell_weights for wi in ci]
         cell_timeseries = [ti for ci in cell_timeseries for ti in ci]
 
     # convert lists to arrays
     cn = len(cell_xyz)
-    block_id = np.array(block_id)
+    cell_block_id = np.array(cell_block_id)
     cell_lengths = np.array([len(i) for i in cell_weights])
     cell_xyz_array = np.full((cn, np.max(cell_lengths), 3), -1, dtype=int)
     cell_weights_array = np.full((cn, np.max(cell_lengths)), np.nan)
@@ -84,4 +84,4 @@ def collect_blocks(color_i, parameters):
         cell_weights_array[ci, :li] = cell_weights[ci]
     cell_timeseries_array = np.array(cell_timeseries)
 
-    return block_id, cell_xyz_array, cell_weights_array, cell_timeseries_array, cell_lengths
+    return cell_block_id, cell_xyz_array, cell_weights_array, cell_timeseries_array, cell_lengths
--- voluseg/_steps/step5.py
+++ voluseg/_steps/step5.py
@@ -26,7 +26,8 @@ def clean_cells(parameters):
         if os.path.isfile(fullname_cells+hdf):
             continue
 
-        block_id, cell_xyz, cell_weights, cell_timeseries, cell_lengths = collect_blocks(color_i, parameters)
+        cell_block_id, cell_xyz, cell_weights, cell_timeseries, cell_lengths = \
+            collect_blocks(color_i, parameters)
 
         fullname_volmean = os.path.join(p.dir_output, 'volume%d'%(color_i))
         with h5py.File(fullname_volmean+hdf, 'r') as file_handle:
@@ -68,7 +69,7 @@ def clean_cells(parameters):
                 cell_valids[pi[np.argmin(cell_w[pi])]] = 0
 
         ## get valid version of cells
-        block_id = block_id[cell_valids]
+        cell_block_id = cell_block_id[cell_valids]
         cell_weights = cell_weights[cell_valids].astype(dtype)
         cell_timeseries = cell_timeseries[cell_valids].astype(dtype)
         cell_lengths = cell_lengths[cell_valids]
@@ -126,7 +127,7 @@ def clean_cells(parameters):
             file_handle['cell_x'] = cell_x
             file_handle['cell_y'] = cell_y
             file_handle['cell_z'] = cell_z
-            file_handle['block_id'] = block_id
+            file_handle['cell_block_id'] = cell_block_id
             file_handle['volume_id'] = volume_id
             file_handle['volume_weight'] = volume_weight
             file_handle['cell_weights'] = cell_weights
