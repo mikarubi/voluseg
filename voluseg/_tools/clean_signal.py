94c3d582805a3011591395d860ed4ebe56a5cb9f
mikarubi <mikarubi@gmail.com>
2021-06-20 17:42:43 -0400

    improved robust detrending

--- voluseg/_tools/clean_signal.py
+++ voluseg/_tools/clean_signal.py
@@ -38,7 +38,7 @@ def clean_signal(parameters, timeseries):
     if p.detrending == 'standard':
         coefpoly = np.polyfit(xtime, timeseries, 2)
     elif p.detrending == 'robust':
-        coefpoly = np.polyfit(xtime, winsorize(timeseries, [0.01, 0.01]), 2)
+        coefpoly = np.polyfit(xtime, winsorize(timeseries, [0.01, 0.01]), 1)
     
     timeseries -= np.polyval(coefpoly, xtime)
     timeseries = np.concatenate((timeseries[::-1], timeseries, timeseries[::-1]))
