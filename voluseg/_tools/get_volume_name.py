7ca26afe2bb63efee18f23cd0537cbd8296cbce2
luiz <luiz_tauffer@hotmail.com>
2024-08-28 14:43:14 +0200

    get_volume_name

--- voluseg/_tools/get_volume_name.py
+++ voluseg/_tools/get_volume_name.py
@@ -1,8 +1,28 @@
-def get_volume_name(fullname_volume, dir_prefix=None, plane_i=None):
-    """get name of volume (with directory prefix and plane suffix)"""
+import os
 
-    import os
 
+def get_volume_name(
+    fullname_volume: str,
+    dir_prefix: str = None,
+    plane_i: int = None,
+) -> str:
+    """
+    Get name of volume (with directory prefix and plane suffix).
+
+    Parameters
+    ----------
+    fullname_volume : str
+        Full name of volume.
+    dir_prefix : str (optional)
+        Prefix for directory. Default is None.
+    plane_i : int (optional)
+        Index of plane. Default is None.
+
+    Returns
+    -------
+    str
+        Name of volume.
+    """
     name_volume = os.path.basename(fullname_volume)
 
     # add prefix (multiple input directories)
