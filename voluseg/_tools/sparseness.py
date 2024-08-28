fd89d5c7061b57c001e64f6996c1fd70c262ab61
luiz <luiz_tauffer@hotmail.com>
2024-08-28 16:16:13 +0200

    sparseness

--- voluseg/_tools/sparseness.py
+++ voluseg/_tools/sparseness.py
@@ -1,8 +1,25 @@
-def sparseness(W, dim=0):
-    """vector sparseness along specified dimension"""
+import numpy as np
 
-    import numpy as np
 
+def sparseness(
+    W: np.ndarray,
+    dim: int = 0,
+):
+    """
+    Vector sparseness along specified dimension.
+
+    Parameters
+    ----------
+    W : np.ndarray
+        Input array.
+    dim : int, optional
+        Dimension along which to compute sparseness, by default 0.
+
+    Returns
+    -------
+    np.ndarray
+        Sparseness of input array along specified dimension.
+    """
     n = W.shape[dim]
     l2 = np.sqrt(np.sum(np.square(W), dim))  # l2-norm
     l1 = np.sum(np.abs(W), dim)  # l1-norm
